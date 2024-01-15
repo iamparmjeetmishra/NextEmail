32f967cd07d86a0705e3f648feeb730cf2cde0a0
Parmjeet <iamparmjeetmishra@gmail.com>
2024-01-15 23:21:31 +0530

    Footer Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Form from '@/components/PageComponents/Form'
-
+const d = new Date();
+let year = d.getFullYear();
 export default function Home() {
   return (
     <section className='container mx-auto'>
@@ -7,6 +8,9 @@ export default function Home() {
         <h2 className='text-5xl font-extrabold mx-auto p-6 bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text '>React Hook Form + Zod</h2>
       </div>
       <Form />
+      <footer className="container font-medium py-8 px-2  mx-auto w-full border-y-2 text-center">
+      © {year} All Rights Reserved | Made with ♡ by <a href="https://parmjeetmishra.com" target="_blank" className="hover:text-cyan-600 " >Parm</a>
+    </footer>
     </section>
   )
 }
